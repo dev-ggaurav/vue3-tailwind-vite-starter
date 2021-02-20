9fa6b321fa8b7d0c8006564a4cfba625d004c7e8
devggaurav <devggaurav@gmail.com>
2021-02-20 13:38:18 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center p-3 mt-32">
+      <h2 class="text-3xl font-bold text-green-500">Hello World!</h2>
+      <p class="text-xl font-semibold text-gray-500">This is a Vue 3 and Tailwind starter project created using Vite.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
